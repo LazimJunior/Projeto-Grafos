a0b32a0f7ff9b029d9b7d713cbe25452b259c0d3
Lezin <lazim.franciscoo@gmail.com>
2025-04-23 18:42:00 -0300

    feat: adicionar integração com interface gráfica PyQt5

--- src/main.py
+++ src/main.py
@@ -1,10 +1,9 @@
-# Importando bibliotecas
-import NetworkX
-import Matplotlib
-import PyQt5
-import os
+import sys
+from PyQt5.QtWidgets import QApplication
+from janela_principal import JanelaPrincipal
 
-#Importando funções 
-#  from *nome-arquivo* import *minha-função*
-
-
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    janela = JanelaPrincipal()
+    janela.show()
+    sys.exit(app.exec_())
